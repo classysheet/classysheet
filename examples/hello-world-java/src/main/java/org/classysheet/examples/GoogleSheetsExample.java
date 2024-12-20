74855aacfc3de09143edd49c79b59d8e4eefe06f
Matheus Garvão <100000464+MatheusGarvao@users.noreply.github.com>
2024-12-20 15:49:24 -0300

    Added google sheets functionality
    Implemented a way to ask for permission on google API on GoogleAuthorizeUtil, needs to refactor it for a better way to give the credentials
    
    GoogleSpreadsheetConnector initialize google sheets service and write the data, but the datetime is not working properly, needs to fix it the type of the date.
    
    ClassySheetService now asks for an ID instead of a fixed one.
    
    Needs to do integration test after credentials fix.
    
    updated google sheets example, but can be refactored it for sheetsService create inside ClassySheetService, but for now, if the user wants to send a custom spreadsheet ID, he cans.

--- examples/hello-world-java/src/main/java/org/classysheet/examples/GoogleSheetsExample.java
+++ examples/hello-world-java/src/main/java/org/classysheet/examples/GoogleSheetsExample.java
@@ -3,16 +3,34 @@ package org.classysheet.examples;
 import org.classysheet.core.api.ClassysheetService;
 import org.classysheet.examples.data.ScheduleGenerator;
 import org.classysheet.examples.domain.Schedule;
+import org.classysheet.core.impl.provider.google.SheetsServiceUtil;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.Spreadsheet;
+import com.google.api.services.sheets.v4.model.SpreadsheetProperties;
 
 public class GoogleSheetsExample {
-
     public static void main(String[] args) {
-        // Once, at build or bootstrap time:
-        ClassysheetService<Schedule> classysheetService = ClassysheetService.create(Schedule.class);
+        try {
+            Sheets sheetsService = SheetsServiceUtil.getSheetsService();
 
-        // At runtime, per workbook:
-        Schedule schedule = ScheduleGenerator.generateDemoData();
-        classysheetService.writeWorkbookToGoogle(schedule);
+            Spreadsheet newSpreadsheet = new Spreadsheet()
+                    .setProperties(new SpreadsheetProperties().setTitle("ClassySheet Example Workbook"));
+            Spreadsheet createdSpreadsheet = sheetsService.spreadsheets().create(newSpreadsheet).execute();
+            String spreadsheetId = createdSpreadsheet.getSpreadsheetId();
+            if (spreadsheetId == null) {
+                throw new RuntimeException("Error creating workbook on google sheets");
+            }
+
+            ClassysheetService<Schedule> classysheetService = ClassysheetService.<Schedule>create(Schedule.class, spreadsheetId);
+
+            Schedule schedule = ScheduleGenerator.generateDemoData();
+
+            classysheetService.writeWorkbookToGoogle(schedule);
+
+            System.out.println("data successfully written on" + spreadsheetId);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
