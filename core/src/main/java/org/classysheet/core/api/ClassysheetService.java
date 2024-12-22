f45d2aafb96623d99d3864d8bcfba8055ff444b8
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2024-12-22 15:05:26 +0100

    move google code into google spreadsheet connector

--- core/src/main/java/org/classysheet/core/api/ClassysheetService.java
+++ core/src/main/java/org/classysheet/core/api/ClassysheetService.java
@@ -1,5 +1,6 @@
 package org.classysheet.core.api;
 
+import org.classysheet.core.api.domain.Workbook;
 import org.classysheet.core.impl.data.SheetData;
 import org.classysheet.core.impl.data.WorkbookData;
 import org.classysheet.core.impl.meta.SheetMeta;
@@ -25,12 +26,18 @@ import java.util.List;
 
 public class ClassysheetService<Workbook_> {
 
+    /**
+     * Create a new service from domain annotations.
+     * Typically called once at bootstrap or build time.
+     *
+     * @param workbookClass never null
+     * @return never null
+     * @param <Workbook_> The class with a {@link Workbook} annotation.
+     */
     public static <Workbook_> ClassysheetService<Workbook_> create(
             Class<Workbook_> workbookClass) {
         return new ClassysheetService<>(new WorkbookMeta(workbookClass));
     }
-
-    private String SPREADSHEET_ID = "19sd5Bw51OsHruzZzkpygXkatBwjSkSxZPZpiW_hzoV8";
 
     protected final WorkbookMeta workbookMeta;
     protected final GoogleSpreadsheetConnector googleSpreadsheetConnector = new GoogleSpreadsheetConnector();
@@ -50,62 +57,7 @@ public class ClassysheetService<Workbook_> {
 
     public void writeWorkbookToGoogle(Workbook_ workbook) {
         WorkbookData workbookData = extractWorkbookData(workbook);
-        Sheets sheetService = null;
-        try {
-            sheetService = SheetsServiceUtil.getSheetsService();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (GeneralSecurityException e) {
-            throw new RuntimeException(e);
-        }
-        Spreadsheet sp = null;
-        try {
-            sp = sheetService.spreadsheets().get(SPREADSHEET_ID).execute();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        List<Sheet> sheets = sp.getSheets();
-        List<String> sheetNames = sheets.stream().map(x -> x.getProperties().getTitle()).toList();
-        for (SheetData sheetData : workbookData.sheetDatas()) {
-            try {
-                String currentSheetName = sheetData.sheetMeta().name();
-                if (!sheetNames.contains(currentSheetName)) {
-                    List<Request> requests = new ArrayList<>();
-                    // Setting the sheet name
-                    AddSheetRequest addSheet = new AddSheetRequest();
-                    addSheet.setProperties(new SheetProperties().setTitle(currentSheetName));
-                    requests.add(new Request().setAddSheet(addSheet));
-                    BatchUpdateSpreadsheetRequest requestBody = new BatchUpdateSpreadsheetRequest().setRequests(requests);
-                    sheetService.spreadsheets().batchUpdate(SPREADSHEET_ID, requestBody).execute();
-                }
-
-                SheetMeta sheetMeta = sheetData.sheetMeta();
-                Values sheetValues = sheetService.spreadsheets().values();
-                List<ValueRange> data = new ArrayList<>();
-                ValueRange columnNames = new ValueRange()
-                        .setRange(currentSheetName + "!A1")
-                        .setValues(List.of(
-                                sheetMeta.columnMetas()
-                                        .stream()
-                                        .map(columnMeta -> (Object) columnMeta.name())
-                                        .toList()
-                        ));
-                data.add(columnNames);
-
-                sheetData.streamRows().forEach((row) -> {
-                    data.add(new ValueRange()
-                            .setRange(currentSheetName + "!A" + (row.index() + 2))
-                            .setValues(List.of(row.readValuesAsString())));
-                    System.out.println("Row: " + row);
-                });
-                BatchUpdateValuesRequest valuesBody = new BatchUpdateValuesRequest()
-                        .setValueInputOption("RAW")
-                        .setData(data);
-                sheetValues.batchUpdate(SPREADSHEET_ID, valuesBody).execute();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        googleSpreadsheetConnector.writeWorkbook(workbookData);
     }
 
     public void writeWorkbookToExcel(Workbook_ workbook) {
--- core/src/main/java/org/classysheet/core/impl/provider/google/GoogleSpreadsheetConnector.java
+++ core/src/main/java/org/classysheet/core/impl/provider/google/GoogleSpreadsheetConnector.java
@@ -1,13 +1,77 @@
 package org.classysheet.core.impl.provider.google;
 
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.*;
+import org.classysheet.core.impl.data.SheetData;
+import org.classysheet.core.impl.meta.SheetMeta;
 import org.classysheet.core.impl.provider.SpreadsheetConnector;
 import org.classysheet.core.impl.data.WorkbookData;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GoogleSpreadsheetConnector implements SpreadsheetConnector {
 
     @Override
     public void writeWorkbook(WorkbookData workbookData) {
-        // TODO
+
+        Sheets sheetService = null;
+        try {
+            sheetService = SheetsServiceUtil.getSheetsService();
+        } catch (IOException | GeneralSecurityException e) {
+            throw new RuntimeException(e);
+        }
+        Spreadsheet sp = null;
+        String SPREADSHEET_ID = "19sd5Bw51OsHruzZzkpygXkatBwjSkSxZPZpiW_hzoV8";
+        try {
+            sp = sheetService.spreadsheets().get(SPREADSHEET_ID).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        List<Sheet> sheets = sp.getSheets();
+        List<String> sheetNames = sheets.stream().map(x -> x.getProperties().getTitle()).toList();
+        for (SheetData sheetData : workbookData.sheetDatas()) {
+            try {
+                String currentSheetName = sheetData.sheetMeta().name();
+                if (!sheetNames.contains(currentSheetName)) {
+                    List<Request> requests = new ArrayList<>();
+                    // Setting the sheet name
+                    AddSheetRequest addSheet = new AddSheetRequest();
+                    addSheet.setProperties(new SheetProperties().setTitle(currentSheetName));
+                    requests.add(new Request().setAddSheet(addSheet));
+                    BatchUpdateSpreadsheetRequest requestBody = new BatchUpdateSpreadsheetRequest().setRequests(requests);
+                    sheetService.spreadsheets().batchUpdate(SPREADSHEET_ID, requestBody).execute();
+                }
+
+                SheetMeta sheetMeta = sheetData.sheetMeta();
+                Sheets.Spreadsheets.Values sheetValues = sheetService.spreadsheets().values();
+                List<ValueRange> data = new ArrayList<>();
+                ValueRange columnNames = new ValueRange()
+                        .setRange(currentSheetName + "!A1")
+                        .setValues(List.of(
+                                sheetMeta.columnMetas()
+                                        .stream()
+                                        .map(columnMeta -> (Object) columnMeta.name())
+                                        .toList()
+                        ));
+                data.add(columnNames);
+
+                sheetData.streamRows().forEach((row) -> {
+                    data.add(new ValueRange()
+                            .setRange(currentSheetName + "!A" + (row.index() + 2))
+                            .setValues(List.of(row.readValuesAsString())));
+                    System.out.println("Row: " + row);
+                });
+                BatchUpdateValuesRequest valuesBody = new BatchUpdateValuesRequest()
+                        .setValueInputOption("RAW")
+                        .setData(data);
+                sheetValues.batchUpdate(SPREADSHEET_ID, valuesBody).execute();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
 }
